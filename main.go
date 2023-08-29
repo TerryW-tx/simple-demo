7abd624f40c77d4b50b450c3c8ba2062871bb575
terry <517557008@qq.com>
2023-08-29 16:59:22 +0800

    debug main.go

--- main.go
+++ main.go
@@ -5,11 +5,16 @@ import (
 	"io"
 	"github.com/RaymondCode/simple-demo/service"
 	"github.com/gin-gonic/gin"
-	"github.com/RaymondCode/simple-demo/model"
+	"github.com/RaymondCode/simple-demo/dal"
+	"gorm.io/gorm"
+        "gorm.io/driver/mysql"
 )
 
 func main() {
-	model.InitDatabase()
+	// model.InitDatabase()
+	gormdb, _ := gorm.Open(mysql.Open("test:123456@(120.55.103.230:3306)/douyin"))
+        // g.UseDB(gormdb) // reuse your gorm db
+	dal.SetDefault(gormdb)
 
 	go service.RunMessageServer()
 
